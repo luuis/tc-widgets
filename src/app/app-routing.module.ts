ff8e8de5316bfa13bd611f2ecfbca98901d2bc18
Luis Enrique Suarez Rosas <luuis.e@outlook.com>
2022-08-16 01:16:59 -0500

    matches widget

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MatchesComponent } from './Widgets/matches/matches.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'matches', component: MatchesComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
