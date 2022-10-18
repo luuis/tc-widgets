0d555d9c177a6a76efeec0009cd65f6458f287e2
Luis Enrique Suarez Rosas <luuis.e@outlook.com>
2022-10-18 18:39:22 -0500

    player preparations + val match in file

--- src/app/Widgets/community-player/community-player.component.ts
+++ src/app/Widgets/community-player/community-player.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  TwitchEmbedLayout,
+  TwitchEmbed,
+  TwitchEmbedTheme,
+} from 'twitch-player';
 
 @Component({
   selector: 'app-community-player',
   templateUrl: './community-player.component.html',
-  styleUrls: ['./community-player.component.scss']
+  styleUrls: ['./community-player.component.scss'],
 })
 export class CommunityPlayerComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    /* const embed = new TwitchEmbed('twitch-embed', {
+      width: 980,
+      height: 480,
+      channel: 'lla',
+      layout: TwitchEmbedLayout.VIDEO_WITH_CHAT,
+      theme: TwitchEmbedTheme.DARK,
+      parent: ['localhost', 'www.esportscruelty.com'],
+    }); */
   }
-
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CalendarComponent } from './Widgets/calendar/calendar.component';
 import { MatchesComponent } from './Widgets/matches/matches.component';
+import { CommunityPlayerComponent } from './Widgets/community-player/community-player.component';
 
 const routes: Routes = [
   { path: 'matches', component: MatchesComponent },
   { path: 'calendar', component: CalendarComponent },
+  { path: 'player', component: CommunityPlayerComponent },
 ];
 
 @NgModule({
